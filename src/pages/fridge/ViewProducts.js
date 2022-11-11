4f956fefd43f1dc80cead3683c5dfcfd9904d6d3
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2022-11-11 18:01:43 +0100

    first layout for the fridge productlist

--- src/pages/fridge/ViewProducts.js
+++ src/pages/fridge/ViewProducts.js
@@ -1,4 +1,4 @@
-import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from '@firebase/firestore';
 import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 import { db } from '../../firebase/config';
@@ -6,6 +6,10 @@ import { FiInfo } from "react-icons/fi"
 import { BiTrash } from "react-icons/bi"
 import { Link } from 'react-router-dom';
 import Loader  from '../../components/loader/Loader';
+
+// styles
+import "./ViewProducts.css"
+
 
 
 const ViewProducts = () => {
@@ -39,6 +43,17 @@ const ViewProducts = () => {
       toast.error (error.message);
     }
   };
+
+  const deleteProduct = async (id) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+      toast.success("Product deleted successfully");
+      
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
+
   return (
     <>
     {isLoading && <Loader/>}
@@ -65,9 +80,8 @@ const ViewProducts = () => {
                     <td>{desc}</td>
                     <td>{amount}</td>
                     <td><Link to ="/"><FiInfo size={20}/></Link></td>
-                    <td><BiTrash size={20} color="red"/></td>
-                  </tr>
-                
+                    <td><BiTrash size={20} color="red" onClick={() => deleteProduct(id)}/></td>
+                  </tr> 
               )
             })}
           </tbody>
